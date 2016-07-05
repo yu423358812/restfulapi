3d943f73f95ef7d09f7def9204166b5bd1cf8f86
yu423358812 <yu23691188@gmail.com>
2016-07-05 11:12:40 -0700

    version2

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,12 @@ module.exports = {
     'facebookAuth' : {
         'clientID': '303733539961269',
         'clientSecret': 'cf46bdfe636cac256b015fece1c4f616',
-        'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL': 'https://tranquil-wind-cave-47165.herokuapp.com/auth/facebook/callback'
     },
 
     'googleAuth' : {
         'clientID': '544691601783-mj0s5jeq4jij4m0v2d01e7jbo5rkdcs1.apps.googleusercontent.com',
         'clientSecret': '8cqVECc9e51TflvluI2DbNoI',
-        'callbackURL': 'http://localhost:3000/auth/google/callback'
+        'callbackURL': 'https://tranquil-wind-cave-47165.herokuapp.com/auth/google/callback'
     }
 }
